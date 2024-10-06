09f22a98112442b80026ef5f7a5f747277450da6
skydiverGM <sonytouchscreen@GMAIL.COM>
2024-10-06 22:19:05 +0200

    Add implementation

--- src/main/java/com/imaks/Main.java
+++ src/main/java/com/imaks/Main.java
@@ -1,7 +1,25 @@
 package com.imaks;
 
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Customer customer = getCustomer(getData());
+        String output = "Customer: " + customer.getName() +
+                " | Phone: " + customer.getPhone();
+        String format = "%s --- %s";
+        String result = String.format(format, LocalDateTime.now(), output);
+        getOutput(result);
     }
+     public static String[] getData(){
+        return new String[]{"Popandopalo", "+380633322211"};
+     }
+
+     public static Customer getCustomer(String[] customer){
+        return new Customer(customer[0], customer[1]);
+     }
+
+     public static void getOutput(String output){
+         System.out.println(output);
+     }
 }
